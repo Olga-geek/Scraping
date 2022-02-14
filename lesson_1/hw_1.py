874680a14e1395d9516165d86807edd4e7572993
Olga-geek <tsurkan.ob@gmail.com>
2022-02-14 22:43:56 +0300

    [upd] lesson_1

--- lesson_1/hw_1.py
+++ lesson_1/hw_1.py
@@ -2,17 +2,20 @@ import requests
 import json
 from pprint import pprint
 
-repos_url = 'https://api.github.com/users/Olga-geek/repos'
+repos_url = 'https://api.github.com'
 
 user_name = 'Olga-geek' #input("Enter the github user_name:")
-response = requests.get(repos_url)
+response = requests.get(f'{repos_url}/users/{user_name}/repos')
 
 j_data = response.json()
 with open('data.txt', 'w') as j_file:
     json.dump(j_data, j_file)
 
-pprint(j_data)
-
+# pprint(j_data)
+num=1
+for i in j_data:
+    print(f"{num} наименование репозитория {i['name']}")
+    num+=1
 
 
 
